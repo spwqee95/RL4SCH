12729fec601363e23fd76bde8b2e0b6508ac4c93
spwqee95 <162087294+spwqee95@users.noreply.github.com>
2025-04-24 22:52:00 -0700

    Update network.py

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class MLPExtractor(BaseFeaturesExtractor):
+    """Simple 2-layer MLP feature block"""
     def __init__(self, obs_space, out_dim=64):
         super().__init__(obs_space, out_dim)
         self.net = nn.Sequential(
